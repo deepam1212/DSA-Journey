90f3ca62372ce03f789229dcb28f6f0a27836c10
Deepam Sharma <deepamsharma@192.168.1.4>
2025-06-16 21:08:47 +0530

    LinkedList Added

---
+++ LinkedList.playground/Contents.swift
@@ -0,0 +1,12 @@
+//MARK: - LinkedList and OOPS Concept
+
+
+/*
+ Programming Paradigm: Style or Standard way of writing a Program
+ 
+ example: mcdonald have a standard way of creating burgers, so all the burgers taster same
+ 
+ 
+ 
+ 
+ */
