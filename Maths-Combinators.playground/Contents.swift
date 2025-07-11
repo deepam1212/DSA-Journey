62b9af75be468c0601f5fac9e075aa3c70a0c729
Deepam Sharma <deepamsharma668@gmail.com>
2025-07-11 21:16:32 +0530

    Added Maths Combinators Notes

---
+++ Maths-Combinators.playground/Contents.swift
@@ -0,0 +1,25 @@
+// MARK: - Maths Combinators
+// Question: Given 10 girls and 7 boys, How many different pairs can be formed
+/*
+ Boys            |              Girls
+ B1              |                G1,G2,G3,G4,G5,G6......G10
+ 
+ 
+ 
+ 
+ 
+ Total pairs = Boys * Girls = 7 * 10 = 70 Pairs
+ 
+ // MARK: - Whenever you see AND -> It means * (Multiplication)
+ 
+ 
+ // MARK: - Number of ways to reach from Pune to Delhi = 6
+           F1             F4
+       /--------\     /---------\
+      /   F2     \   /    F5     \
+ Pune ----------Delhi-----------Agra
+      \          /
+       \________/
+           F3
+ */
+
