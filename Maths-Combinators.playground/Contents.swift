0caae3eabf9673e1d076c27faf492130362998c9
Deepam Sharma <deepamsharma668@gmail.com>
2025-07-11 21:48:33 +0530

    Added Permutation and combination for this

--- Maths-Combinators.playground/Contents.swift
+++ Maths-Combinators.playground/Contents.swift
@@ -21,5 +21,61 @@
       \          /
        \________/
            F3
+ 
+ // MARK: - If the conditions are OR then +(Addition)
  */
+// MARK: - Permutation
 
+/* Given 3 character distict character. How many ways can we arrange them.
+
+abc
+ 
+abc
+acb
+
+bac
+bca
+ 
+cba
+cab
+ 
+ Total 6
+ 
+ Question
+ 
+ String = Date
+ 
+ N = 4, Combination to be made of 2 char pairs
+ 
+ d -> a,t,e
+ a -> d,t,e
+ t -> d,a,e
+ e -> d,a,t
+ 
+ 4*3 = 12 Pairs
+ 
+ n n-1 n-2 n-3       n-r-1
+ 
+ r is the rth position
+ 
+ n * (n-1) * (n-2) : (n-r-1)
+ 
+ Numnber of ways to arrange objects from N Distinct Objects = n! / (n-r)!
+ 
+ DATE = n = 4, r = 2
+ 4!/ (4-2)! = 4*3*2*1/ 2*1 = 4*3 = 12 Pairs
+ 
+ // MARK: - In Permutation the order matters, in COmbination the order does not matter
+ 
+ // MARK: - Combination
+ Question: Given 4 Players, How many ways to select 3 player
+ 
+ {p1 p2 p3 p4}
+ 
+ p1 p2 p3                   --
+ p1 p3p p4                    |_ Total 4 Combination
+ p1 p2 p4                     |
+ p2 p3p p4                  --
+ 
+ 
+*/
