cdcd9616ae300eb34cb8e7418729ba3495609f98
Deepam Sharma <deepamsharma668@gmail.com>
2025-07-13 02:28:01 +0530

    Added Frequency QUestion and Unique Element Question using Dictionary

--- HashMap.playground/Contents.swift
+++ HashMap.playground/Contents.swift
@@ -44,6 +44,50 @@ print(dict2)
 // MARK: - Dictionary is a value type and still stores memory in heap(and same goes for array)
 // Dictionary is a value type (a struct), but it stores its data (keys and values) in the heap.
 
+// MARK: - Anything we do in a dicgionafry like appending, removing, searching it is O(1)
+func frequencyCalculator() {
+    var arrNew: [Int] = [10,10,20,30,40,50,40,40]
+    var query: [Int] = [10,40]
+    //
+    var seen: [Int: Int] = [:]
+    //
+    for item in arrNew {
+        if var myValue = seen[item] {//[10:2, 20: 1]
+            seen[item] = myValue + 1
+        } else {
+            seen[item] = 1//[10:1]
+        }
+    }
+    //
+    for item in seen {
+        if item.value == 1 {
+            print(item.key)
+        }
+    }
+    //
+//    for item in query {
+//        if var myValue = seen[item] {
+//            print(myValue)
+//        }
+//    }
+}
+frequencyCalculator()
 
 
+var dictName: [String: Int] = ["Agrima": 1, "Deepam": 2]
+for i in 0...1 {
+    if let myValue = dictName["Agria"] {
+        print("MyValue", myValue)
+    } else {
+        print("No Value with Agria Key")
+        dictName["Agria"] = 3
+    }
+}
+var arrDict: [[String: Int]] = [["Agrima": 1, "Deepam": 2], ["Dirhghayu": 3, "Shubham": 4]]
+print(arrDict)
+struct DiamondModal {
+    let diamondId: Int = 0
+    let diamondName: String = "Rose"
+}
 
+var arrDiamond: [DiamondModal] = []
